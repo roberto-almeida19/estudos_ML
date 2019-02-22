230426e756e35f74e34b06932e704090d35447a8
Roberto Almieda <scorpions2007@hotmail.com>
2019-02-22 17:10:13 -0300

    Começando o cap 4

---
+++ 4/classifica_cursos.py
@@ -0,0 +1,38 @@
+
+from estudos_ML.tratador import leitor
+X,Y = leitor.carregar_arquivo()
+
+import pandas as pd
+dataFrame = pd.read_csv('cursos.csv')
+X_df = dataFrame[['home', 'busca', 'logado']]
+Y_df = dataFrame['comprou']
+Xdummies_df = pd.get_dummies(X_df)
+Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+porcentagem_treino = 0.9
+tamanho_treino = int(porcentagem_treino*len(Y))
+tamanho_teste = len(Y) - tamanho_treino
+
+treino_dados = X[:tamanho_treino]
+treino_marcacoes = Y[:tamanho_treino]
+
+teste_dados = X[-tamanho_teste:]
+teste_marcacoes = Y[-tamanho_teste:]
+
+
+from sklearn.naive_bayes import MultinomialNB
+modelo = MultinomialNB()
+modelo.fit(treino_dados,treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+
+diferencas = resultado - teste_marcacoes
+acertos = [d for d in diferencas if d == 0]
+total_acertos = len(acertos)
+total_elementos = len(teste_dados)
+taxa_acerto = 100.0* total_acertos / total_elementos
+print('Elementos',total_elementos)
+print('Acertos',total_acertos)
